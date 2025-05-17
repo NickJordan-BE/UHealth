6824f24bed8048c0a3caa2992c21cadc6a957e85
Anthony <antnay@uw.edu>
2025-05-17 13:51:20 -0700

    feat: big stuff

--- server/server.py
+++ server/server.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from routes import setup
+from db import db
 
 app = Flask(__name__)
+setup(app)
+db.connect_db()
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8034)
